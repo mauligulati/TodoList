d9bfddd1dd46fb377851450502d018c530f8ab6d
Mauli <mauligulati@gmail.com>
2025-07-21 18:05:55 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Mauli Gulati on 21/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
