577a9a83dda6b418d52a1078b8003420aa9503cc
Mauli <mauligulati@gmail.com>
2025-07-21 18:54:43 +0800

    [Feature] Add ListView

---
+++ TodoList/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  TodoList
+//
+//  Created by Mauli Gulati on 21/7/25.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
