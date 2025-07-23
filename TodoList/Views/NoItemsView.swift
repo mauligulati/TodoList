71207b10bbd39329311b41006f5622a60b709c5b
Mauli <mauligulati@gmail.com>
2025-07-23 20:34:10 +0800

    [Feature] Add NoItemsView, Add animation, Add new color set

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,74 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Mauli Gulati on 23/7/25.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State private var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 10) {
+                    Text("There are no items!")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    Text("Please click on the add button and add a bunch of items to your todo list.")
+                        .padding(.bottom, 20)
+                    NavigationLink("Add 🥳") {
+                        AddView()
+                    }
+                    .foregroundStyle(.white)
+                    .font(.headline)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(animate ? secondaryAccentColor : Color.accentColor)
+                    )
+                    .padding(.horizontal, animate ? 30 : 50)
+                    .shadow(
+                        color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                        radius: animate ? 30 : 10,
+                        x: 0,
+                        y: animate ? 50 : 30
+                    )
+                    .scaleEffect(animate ? 1.1 : 1.0)
+                    .offset(y: animate ? -7 : 0)
+                }
+                .multilineTextAlignment(.center)
+                .padding(40)
+                .onAppear(perform: addAnimation)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .navigationTitle("Title")
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+                ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        NoItemsView()
+    }
+    .navigationTitle("Title")
+    
+}
