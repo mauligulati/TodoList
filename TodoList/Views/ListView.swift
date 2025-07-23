59faeb8e56d7444bc2b063fde0f996d2ff8079ad
Mauli <mauligulati@gmail.com>
2025-07-23 14:27:35 +0800

    [Feature] Add delete and move functionality

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -15,24 +15,41 @@ struct ListView: View {
         Item(title: "Third Item!", isCompleted: false)
     ]
     
+    @State private var editMode: EditMode = .inactive
+
+    
     var body: some View {
         List {
             ForEach(items) { item in
                 ListRowView(item: item)
             }
+            .onDelete(perform: deleteItem)
+            .onMove(perform: moveItem)
         }
         .listStyle(.plain)
         .navigationTitle("Todo List 📝")
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
-                Button("Edit") {
-                    
+                Button(editMode == .active ? "Done" : "Edit") {
+                    withAnimation {
+                        editMode = (editMode == .active) ? .inactive : .active
+                    }
                 }
             }
             ToolbarItem(placement: .topBarTrailing) {
                 NavigationLink("Add", destination: AddView())
             }
         }
+        .environment(\.editMode, $editMode) // ✅ this connects the list to the custom edit mode
+
+    }
+    
+    func deleteItem(indexSet:  IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    
+    func moveItem(from: IndexSet, to: Int) {
+        items.move(fromOffsets: from, toOffset: to)
     }
 }
 
